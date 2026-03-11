7d5efe3e451b1a713133f57aa84c8e2370c7d119
Anushka Katole <katoleanushka@gmail.com>
2026-03-11 18:42:36 +0530

    fix app

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 
 @st.cache_resource
 def load_model():
-    return joblib.load("laptop_price_model.pkl")
+    return joblib.load("scaler.pkl")
 
 @st.cache_data
 def load_metadata():
